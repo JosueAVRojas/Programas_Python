1d89e080c6e00970a979a05597e23771571feeaf
JosueAVRojas <josuealfredovrojas@outlook.com>
2022-04-07 18:22:35 -0500

    Ejercicio 2 terminado

---
+++ Ejercicio2.py
@@ -0,0 +1,11 @@
+# Almacenar en un vector los numero 5,3,7,2,6,3
+# Determinar el numero mayor
+
+numeros = [5,3,7,2,6,3]
+mayor = numeros[0]
+
+for num in numeros:
+
+    if(num > mayor):
+        mayor = num
+        print("El numero mayor es: ", mayor)
