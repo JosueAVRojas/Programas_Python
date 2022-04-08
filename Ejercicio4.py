619c007e64bcb362cb1c0abc3774e732009248a2
JosueAVRojas <josuealfredovrojas@outlook.com>
2022-04-07 19:31:50 -0500

    Ejercicio 4 terminado

---
+++ Ejercicio4.py
@@ -0,0 +1,27 @@
+# Almacenar en un arreglo los numeros: 5,4,7,2,8,4,6
+# Obtener media, desviacion estandar y varianza
+#numeros = [5,4,7,2,8,4,6]
+numeros = [8.5,7.0,9.5,8.0,9.0,8.8,7.5,10]
+suma = 0
+cont = 0
+varianza = 0
+sumVar = 0
+prom = 0
+
+for num in numeros:
+    suma = suma + num
+    cont = cont + 1
+prom = suma / cont
+
+for num in numeros:
+    varianza = (num - prom)
+    varianza = varianza ** 2
+varianza = varianza / cont
+
+
+
+print("La media del vector es: ", suma / cont)
+print("La desviacion estandar del vector es: ", pow(varianza,2))
+print("Lavarianza del vector es: ", varianza)  
+
+
